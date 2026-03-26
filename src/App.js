c8168d8b3a5af7ffc3f60f8794c6f49bc4ac7593
Sabiqunnahar <sabiqunnahar65@gmail.com>
2026-03-26 19:39:23 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from './Components/Header'; 
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <>
-    <h1>Ecommerce website in react js</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Header/index.js
@@ -0,0 +1,19 @@
+const Header =()=>{
+    return(
+        <>
+            <div className="headerWrapper">
+                <div className="top-strip bg-blue">
+                    <div className="container">
+                        <p className="mb-0 mt-0 text-center"><b><b>📣 Affordable and comfortable shopping 📣</b></b> </p>
+                    </div>
+                </div>
+
+                <div className="header">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
