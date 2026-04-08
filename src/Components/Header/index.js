82784c379a6752560e68987dff2c308ee67cecf6
Sabiqunnahar <sabiqunnahar65@gmail.com>
2026-04-08 13:03:04 +0600

    complete header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,15 +4,17 @@ import CountryDropdown from "../CountryDropdown";
 import { IoIosSearch } from "react-icons/io";
 import Button from '@mui/material/Button';
 import { FiUser } from "react-icons/fi";
+import { FiShoppingCart } from "react-icons/fi";
 
 
-const Header =()=>{
-    return(
+const Header = () => {
+    return (
         <>
             <div className="headerWrapper">
                 <div className="top-strip bg-blue">
                     <div className="container">
-                        <p className="mb-0 mt-0 text-center"><b><b>📣 Affordable and comfortable shopping 📣</b></b> </p>
+                        {/* Fixed: Removed redundant nested <b> tags */}
+                        <p className="mb-0 mt-0 text-center"><b>📣 Affordable and comfortable shopping 📣</b></p>
                     </div>
                 </div>
 
@@ -20,26 +22,33 @@ const Header =()=>{
                     <div className="container">
                         <div className="row">
                             <div className="logoWrapper d-flex align-items-center col-sm-2">
-                                <Link to={'/'}><img src={logo} alt='Logo'/></Link>
+                                <Link to={'/'}><img src={logo} alt='Logo' /></Link>
                             </div>
 
                             <div className='col-sm-10 d-flex align-items-center part2 '>
-                               <CountryDropdown/>
+                                <CountryDropdown />
 
-                               {/*Header Search Start Here */}
-                                   <div className='headerSearch ml-3 mr-3'>
-                                       <input type='text' placeholder='Search for products....'/>
-                                       <Button><IoIosSearch /></Button>
-                                   </div>
-                               {/*Header Search ends Here */}
+                                {/*Header Search Start Here */}
+                                <div className='headerSearch ml-3 mr-3'>
+                                    <input type='text' placeholder='Search for products....' />
+                                    <Button><IoIosSearch /></Button>
+                                </div>
+                                {/*Header Search ends Here */}
 
-                               <div className='part3 d-flex align-items-center'>
-                                   <Button className='circle'><FiUser/></Button>
-                               </div>
+                                <div className='part3 d-flex align-items-center ml-auto'>
+                                    <Button className='circle mr-3'><FiUser /></Button>
+                                    <div className='ml-auto carTab d-flex align-items-center'>
+                                        <span className='price'>৳329</span>
+                                        <div className='position-relative ml-2'>
+                                            <Button className='circle'><FiShoppingCart /></Button>
+                                            <span className='count d-flex align-items-center justify-content-center'>1</span>
+                                        </div>
+                                    </div>
+                                </div>
 
                             </div>
                         </div>
-                    </div> 
+                    </div>
                 </header>
             </div>
         </>
